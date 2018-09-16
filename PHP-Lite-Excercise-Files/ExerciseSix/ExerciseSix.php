f87bc55f8b592b445051ce22c2bcaad810643af6
Allen Welch <allenjwelch@gmail.com>
2018-09-16 12:08:57 -0400

    wip 9.16

--- PHP-Lite-Excercise-Files/ExerciseSix/ExerciseSix.php
+++ PHP-Lite-Excercise-Files/ExerciseSix/ExerciseSix.php
@@ -19,6 +19,13 @@
 		*/
 		// there is no reason to create a variable that we only need
 		// to control the if
+
+		/*
+		include "filename"
+		inclde_once "filename"
+		require "filename" ---> throws fatal error if file cannot be included
+		require_once "filename" ---> throws fatal error if file cannot be included
+		*/
 		if (ValidateUser("sconnor","fatethatwemake"))
 		{
 			echo "Welcome<br>";
@@ -27,6 +34,35 @@
 		{
 			echo "Please try again.<br>";
 		}
+
+		echo "<br>";
+		// network function returns the IP for a host name
+		$name = "cnn.com";
+		$ipAddr = gethostbyname($name);
+		echo "This is the IP address of $name: $ipAddr <br>";
+
+		echo "<br>";
+		// network function returns the host name for an IP address
+		$name = gethostbyaddr($ipAddr);
+		echo "This is the host address of $ipAddr: $name<br>";
+
+		echo "<br>";
+		//network function returns the DNS record for a host name
+		$dnsRec = dns_get_record($name);
+		print_r($dnsRec);
+
+		echo "<br><br>";
+		// string function to replace / with ,
+		$bmonth = '1';
+		$bdate = '1';
+		$byear = '1995';
+
+		echo "<br>";
+		// Calendar function to lookup day of week for a date
+		$bday = juliantojd($bdate, $bmonth, $byear);
+		echo "Your birthday $bdate/$bmonth/$byear fell on a " . jddayofweek($bday, 1);
+
+
 	?>
 </body>
 </html>
