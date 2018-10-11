29880f1f6c1625cc007bbe474aee89771ec6b011
Allen Welch <allenjwelch@gmail.com>
2018-10-10 22:28:30 -0400

    updated with password encrpyt

--- PHP for Beginners/_practical-app/7.php
+++ PHP for Beginners/_practical-app/7.php
@@ -1,34 +1,51 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
-    
+
 
 	<section class="content">
 
 		<aside class="col-xs-4">
 
 		<?php Navigation();?>
-			
-			
+
 		</aside><!--SIDEBAR-->
 
 
 	<article class="main-content col-xs-8">
-	
-	
-	
-	<?php  
 
+	<?php
 	/*  Step 1 - Create a database in PHPmyadmin
-
 		Step 2 - Create a table like the one from the lecture
-
 		Step 3 - Insert some Data
-
 		Step 4 - Connect to Database and read data
+	*/
 
-*/
-	
+		$connection = mysqli_connect('localhost', 'root', 'root', 'practice7');
+		if(!$connection) {
+			die("Database connection failed");
+		}
+
+		if(isset($_POST['submit'])) {
+
+			$hero = $_POST['hero'];
+			$power = $_POST['power'];
+
+			$query = "INSERT INTO heros(Name, Superpower) ";
+			$query .= "VALUES ('$hero', '$power') ";
+			$result = mysqli_query($connection, $query);
+
+			if(!$result) {
+				die('Query FAILED' . mysqli_error($connection));
+			}
+		}
 	?>
+
+	<h1>Create your Team!</h1>
+	<form action="7.php" method="post">
+		<input type="text" name="hero" placeholder="Hero">
+		<input type="text" name="power" placeholder="Superpower">
+		<input type="submit" name="submit" value="GO!">
+	</form>
 
 
 
--- PHP for Beginners/new-lecture-files- HERE/_36_mysql/functions.php
+++ PHP for Beginners/new-lecture-files- HERE/_36_mysql/functions.php
@@ -1,16 +1,16 @@
-<?php include "db.php"; ?> 
+<?php include "db.php"; ?>
 
-<?php 
+<?php
 
   function showAllData() {
-    global $connection; 
-    $query = "SELECT * FROM users"; 
+    global $connection;
+    $query = "SELECT * FROM users";
     $result = mysqli_query($connection, $query);
-    
+
     if(!$result) {
       die('Query FAILED' . mysqli_error());
     }
-  
+
     while($row = mysqli_fetch_assoc($result)) {
       $id = $row['id'];
       echo "<option value='$id'>$id</option>";
@@ -19,11 +19,22 @@
   }
 
   function createUser() {
-    global $connection; 
+    global $connection;
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $query = "INSERT INTO users(username, password) "; 
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+
+    $hashFormat = "$2y$10$";
+
+    $salt = "iusesomecrazystrings22";
+
+    $hashF_and_salt = $hashFormat . $salt;
+
+    $password = crypt($password, $hashF_and_salt);
+
+    $query = "INSERT INTO users(username, password) ";
     $query .= "VALUES ('$username', '$password') ";
     $result = mysqli_query($connection, $query);
 
@@ -33,34 +44,34 @@
   }
 
   function UpdateTable() {
-    global $connection; 
-    $username = $_POST['username']; 
-    $password = $_POST['password']; 
-    $id = $_POST['id']; 
+    global $connection;
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $id = $_POST['id'];
 
-    $query = "UPDATE users SET "; 
-    $query .= "username = '$username', "; 
-    $query .= "password = '$password' "; 
+    $query = "UPDATE users SET ";
+    $query .= "username = '$username', ";
+    $query .= "password = '$password' ";
     $query .= "WHERE id = $id ";
-    
-    $result = mysqli_query($connection, $query); 
+
+    $result = mysqli_query($connection, $query);
     if(!$result) {
-      die("QUERY FAILED" . mysqli_error($connection)); 
+      die("QUERY FAILED" . mysqli_error($connection));
     }
   }
 
   function deleteRows() {
-    global $connection; 
-    $username = $_POST['username']; 
-    $password = $_POST['password']; 
-    $id = $_POST['id']; 
+    global $connection;
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $id = $_POST['id'];
 
-    $query = "DELETE FROM users "; 
+    $query = "DELETE FROM users ";
     $query .= "WHERE id = $id ";
-    
-    $result = mysqli_query($connection, $query); 
+
+    $result = mysqli_query($connection, $query);
     if(!$result) {
-      die("QUERY FAILED" . mysqli_error($connection)); 
+      die("QUERY FAILED" . mysqli_error($connection));
     }
   }
 
